5113e5a71707249bdaee539e68716a9710d8f7bc
Jun Aishima <jaishima@bnl.gov>
2020-02-25 13:35:48 -0500

    mountCounter.py should not be logged
     * print out directly!

--- mountCounter.py
+++ mountCounter.py
@@ -1,13 +1,11 @@
 #!/opt/conda_envs/lsdc_dev/bin/python
 import lsdb1
 import sys
-import logging
-logger = logging.getLogger(__name__)
 
 startDate = sys.argv[1]
 endDate = sys.argv[2]
 beamline = sys.argv[3]
 
 m = lsdb1.getMountCount(startDate,endDate,beamline)
-logger.info(m)
+print(m)
 

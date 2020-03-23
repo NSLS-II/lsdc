702e800493362e47dda425b8cc2527f947ff87d5
Jun Aishima <jaishima@bnl.gov>
2020-03-23 17:33:58 -0400

    fix up DB creator
     * user now required with many calls
     * remove unnecessary lines

--- dev_utils/createTestDB.py
+++ dev_utils/createTestDB.py
@@ -1,41 +1,45 @@
-#!/usr/bin/env python
-####!/usr/bin/python
-from __future__ import (absolute_import, print_function)
 
-#from odm_templates import collections
+import time
+import os
 
-#import lsdc.db_lib  # makes db connection
+import sys
+
+import traceback
+
+from mongoengine import NotUniqueError
+
 from db_lib import *  # makes db connection
 
 
 def createTestDB():
         db_connect()
-#        createBeamline("fmx", "17id1")
+        createBeamline("fmx", "17id1")
 #        createBeamline("amx", "17id2")        
 #        createBeamline("john", "99id1")
+        owner = 'john'
         
         
         # containers
         for i in range(1,5):  # 1 indexed, discontinuity for testing
             containerName = 'Puck_{0}'.format(i)
-            createContainer(containerName, 16, kind='16_pin_puck')
+            createContainer(containerName, 16, owner, kind='16_pin_puck')
 
         for i in range(1,5):  # discontinuity for testing
             containerName = 'dewar_{0}'.format(i)
-            createContainer(containerName, 100,kind='shipping_dewar') # I don't know dewar capacity
+            createContainer(containerName, 100, owner, kind='shipping_dewar') # I don't know dewar capacity
 
 
         # named containers
         primary_dewar_name = 'primaryDewar'
 
-        createContainer("primaryDewar", 24,kind='24_puck_robot_dewar')
-        createContainer("primaryDewarAMX", 24,kind='24_puck_robot_dewar')
-        createContainer("primaryDewarFMX", 24,kind='24_puck_robot_dewar')
+        createContainer("primaryDewar", 24, owner, kind='24_puck_robot_dewar')
+        createContainer("primaryDewarAMX", 24, owner, kind='24_puck_robot_dewar')
+        createContainer("primaryDewarFMX", 24, owner, kind='24_puck_robot_dewar')
         
 
         for i in range(1,5):  # discontinuity for testing
             containerName = 'Puck_{0}'.format(i)
-            insertIntoContainer(primary_dewar_name, i, getContainerIDbyName(containerName))
+            insertIntoContainer(primary_dewar_name, owner, i, getContainerIDbyName(containerName, 'john'))
 
 
         # samples
@@ -46,17 +50,17 @@ def createTestDB():
                 sampleName = 'samp_{0}_{1}'.format(i, j)
 
                 try:
-                    sampID = createSample(sampleName, sample_type=type_name)
+                    sampID = createSample(sampleName, owner, kind='24_puck_robot_dewar', sample_type=type_name)
 
                 except NotUniqueError:
                     raise NotUniqueError('{0}'.format(sampleName))
 
-                if not insertIntoContainer(containerName, j, sampID):
+                if not insertIntoContainer(containerName, owner, j, sampID):
                     print('name {0}, pos {1}, sampid {2}'.format(containerName, j, sampID))
 
 
 
-        beamlineInfo('john', 'mountedSample', info_dict={'puckPos': 0, 'pinPos': 0,
+        beamlineInfo('fmx', 'mountedSample', info_dict={'puckPos': 0, 'pinPos': 0,
                                                          'sampleID': -99})
 
 

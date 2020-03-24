664e00ff85e378cdfe85eb33d3d78eb807f0b244
Jun Aishima <jaishima@bnl.gov>
2020-03-24 10:13:41 -0400

    add all beamline config parameters necessary to start lsdcServer

--- dev_utils/createTestDB.py
+++ dev_utils/createTestDB.py
@@ -13,7 +13,8 @@ from db_lib import *  # makes db connection
 
 def createTestDB():
         db_connect()
-        createBeamline("fmx", "17id1")
+        beamline = "fmx"
+        createBeamline(beamline, "17id1")
 #        createBeamline("amx", "17id2")        
 #        createBeamline("john", "99id1")
         owner = 'john'
@@ -60,9 +61,55 @@ def createTestDB():
 
 
 
-        beamlineInfo('fmx', 'mountedSample', info_dict={'puckPos': 0, 'pinPos': 0,
-                                                         'sampleID': -99})
+        #setBeamlineConfigParam(beamline, 'mountedSample', {'puckPos': 0, 'pinPos': 0, 'sampleID': -99}) #mountedSample has no val... why??
+        beamlineInfo(beamline, 'mountedSample', {'puckPos': 0, 'pinPos': 0, 'sampleID': -99})
+        setBeamlineConfigParam(beamline, 'dewarPlateMap', {'0':[180,-180], '1':[135,225], '2':[90,-270], '3':[45,-315], '4':[0,360], '5':[315,-45], '6':[270,90], '7':[225,-135]})
+        setBeamlineConfigParam(beamline, 'dewarPlateName', 'dewarPlateJohn')
+        setBeamlineConfigParam(beamline, 'lowMagCamURL', 'http://xf17id1c-ioc2.cs.nsls2.local:8007/C2.MJPG.mjpg')
+        setBeamlineConfigParam(beamline, 'highMagCamURL', 'http://xf17id1c-ioc2.cs.nsls2.local:8008/C2.MJPG.mjpg')
+        setBeamlineConfigParam(beamline, 'highMagZoomCamURL', 'http://xf17id1c-ioc2.cs.nsls2.local:8008/C1.MJPG.mjpg')
+        setBeamlineConfigParam(beamline, 'lowMagZoomCamURL', 'http://xf17id1c-ioc2.cs.nsls2.local:8007/C3.MJPG.mjpg')
+        setBeamlineConfigParam(beamline, 'lowMagFOVx', '1450')
+        setBeamlineConfigParam(beamline, 'lowMagFOVy', '1160')
+        setBeamlineConfigParam(beamline, 'highMagFOVx', '380')
+        setBeamlineConfigParam(beamline, 'highMagFOVy', '300')
+        setBeamlineConfigParam(beamline, 'lowMagPixX', '640')
+        setBeamlineConfigParam(beamline, 'lowMagPixY', '512')
+        setBeamlineConfigParam(beamline, 'highMagPixX', '640')
+        setBeamlineConfigParam(beamline, 'highMagPixY', '512')
+        setBeamlineConfigParam(beamline, 'screenPixX', '640')
+        setBeamlineConfigParam(beamline, 'screenPixY', '512')
+        setBeamlineConfigParam(beamline, 'beamlineComm', 'XF:17IDC-ES:FMX{Comm}')
+        setBeamlineConfigParam(beamline, 'gonioPvPrefix', 'XF:17IDC-ES:FMX')
+        setBeamlineConfigParam(beamline, 'detector_id', 'EIGER-16')
+        setBeamlineConfigParam(beamline, 'detRadius', '116.0')
+        setBeamlineConfigParam(beamline, 'detector_type', 'pixel_array')
+        setBeamlineConfigParam(beamline, 'imgsrv_port', '14007')
+        setBeamlineConfigParam(beamline, 'imgsrv_host', 'x25-h.nsls.bnl.gov')
+        setBeamlineConfigParam(beamline, 'has_edna', '1')
+        setBeamlineConfigParam(beamline, 'has_beamline', '0')
+        setBeamlineConfigParam(beamline, 'detector_offline', '0')
+        setBeamlineConfigParam(beamline, 'has_xtalview', '1')
+        setBeamlineConfigParam(beamline, 'camera_offset', '0.0')
+        setBeamlineConfigParam(beamline, 'xtal_url_small', 'http://xf17id1c-ioc2.cs.nsls2.local:8008/C2.MJPG.jpg')
+        setBeamlineConfigParam(beamline, 'xtal_url', 'http://xf17id1c-ioc2.cs.nsls2.local:8007/C2.MJPG.jpg')
+        setBeamlineConfigParam(beamline, 'mono_mot_code', 'mon')
+        setBeamlineConfigParam(beamline, 'screen_default_protocol', 'Screen')
+        setBeamlineConfigParam(beamline, 'screen_default_phist', '0.0')
+        setBeamlineConfigParam(beamline, 'screen_default_phi_end', '0.2')
+        setBeamlineConfigParam(beamline, 'screen_default_width', '0.2')
+        setBeamlineConfigParam(beamline, 'screen_default_dist', '137.0')
+        setBeamlineConfigParam(beamline, 'screen_default_time', '0.02')
+        setBeamlineConfigParam(beamline, 'screen_default_reso', '2.0')
+        setBeamlineConfigParam(beamline, 'screen_default_wave', '1.0')
+        setBeamlineConfigParam(beamline, 'screen_default_energy', '13.0')
+        setBeamlineConfigParam(beamline, 'screen_default_beamWidth', '30')
+        setBeamlineConfigParam(beamline, 'screen_default_beamHeight', '30')
+        setBeamlineConfigParam(beamline, 'stdTrans', '0.1')
+        setBeamlineConfigParam(beamline, 'beamstop_x_pvname', 'beamStopX')
+        setBeamlineConfigParam(beamline, 'beamstop_y_pvname', 'beamStopY')
 
+        
 
 
 if __name__ == '__main__':

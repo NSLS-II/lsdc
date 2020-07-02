753141a5cfbfe1032cb919bfa2907d9fcb286691
Jun Aishima <jaishima@bnl.gov>
2020-07-02 10:04:12 -0400

    update lsdcServer call
     * remove unnecessary code
     * remove unnecessary libraries in PYTHONPATH
     * add python3 albula to PYTHONPATH
    
    Conflicts:
    	bin/lsdcServerPI_Merge.cmd

--- bin/lsdcServerPI_Merge.cmd
+++ bin/lsdcServerPI_Merge.cmd
@@ -1,14 +1,9 @@
 #!/bin/bash -l
 export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/
 export CONFIGDIR=${PROJDIR}bnlpx_config/
-#export LSDCHOME=${PROJDIR}lsdcEiger2
 export LSDCHOME=${PROJDIR}lsdc
-export PYTHONPATH=".:${CONFIGDIR}:/usr/local/albula_py3/dectris/albula/3.1/python:/usr/local/edna-mx/mxv1/src:/usr/local/edna-mx/kernel/src:${LSDCHOME}:${PROJDIR}/RobotControlMerge"
-export PATH=/opt/conda/bin/:/usr/local/bin:/usr/bin:/bin:${PROJDIR}/software/bin:/GPFS/CENTRAL/XF17ID1/usr/local/crys/ccp4-6.5/bin
-# source my nfs home bash if we can get too it
-#if [ -e /nfs/skinner/.bashrc ]; then
-#    . /nfs/skinner/.bashrc
-#fi
+export PYTHONPATH=".:${CONFIGDIR}:/usr/local/albula_py3/dectris/albula/3.2/python:/usr/local/edna-mx/mxv1/src:/usr/local/edna-mx/kernel/src:${LSDCHOME}:${PROJDIR}/RobotControlMerge"
+export PATH=/usr/local/bin:/usr/bin:/bin:${PROJDIR}/software/bin:/GPFS/CENTRAL/XF17ID1/usr/local/crys/ccp4-6.5/bin
 source ${CONFIGDIR}daq_env.txt
 export matlab_distrib=${PROJDIR}/software/c3d/matlab_distrib
 export LD_LIBRARY_PATH=$matlab_distrib/bin/glnx86:$matlab_distrib/toolbox

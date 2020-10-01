6e250f18a27aa302ac82ff1bbac1aa811858ce4c
dkreitler <dkreitler@bnl.gov>
2020-10-01 16:50:14 -0400

    runDozorThread with snakeRasterNormal, works with 1 row

--- daq_macros.py
+++ daq_macros.py
@@ -1457,17 +1457,15 @@ def snakeRasterNormal(rasterReqID,grain=""):
         seqNum = int(det_lib.detector_get_seqnum())
       else:
         seqNum = -1
-        _thread.start_new_thread(runDialsThread,(data_directory_name,filePrefix+"_Raster",i,numsteps,seqNum))
-       #time.sleep(2) #make up for lack of _thread join() method
+      _thread.start_new_thread(runDozorThread,(data_directory_name,filePrefix+"_Raster",i,numsteps,seqNum))
+      time.sleep(2) #make up for lack of _thread join() method
   det_lib.detector_stop_acquire()
-  det_lib.detector_wait()
+  det_lib.detector_wait()  
 
-  
-
-  for k in range(0,rasterDef['rowDefs']):
+  #for k in range(0,rasterDef['rowDefs']):
     #_thread.start_new_thread(runDozorThread,(data_directory_name,filePrefix+"_Raster",k,numsteps,seqNum))
     #time.sleep(2)#for lack of join()
-    print(k)
+    #print(k)
   #det_lib.detector_stop_acquire()
   #det_lib.detector_wait()
   logger.info('detector finished waiting')

3d19f408ae7d901591a6681150f0df6d89cd2d2d
Jun Aishima <jaishima@bnl.gov>
2021-01-06 14:37:42 -0500

    add kinds to zebra attributes
     * provide useful feedback when queried
     * fixed spelling of ARRAY_ACQ PV

--- zebra.py
+++ zebra.py
@@ -2,49 +2,48 @@ from ophyd import (Component as Cpt, EpicsSignal, EpicsSignalRO, Device)
 
 
 class ZebraPCBase(Device):
-    sel = Cpt(EpicsSignal, 'SEL')
-    start = Cpt(EpicsSignal, 'START')
-    width = Cpt(EpicsSignal, 'WID')
-    step = Cpt(EpicsSignal, 'STEP')
+    sel = Cpt(EpicsSignal, 'SEL', kind="config", auto_monitor=True)
+    start = Cpt(EpicsSignal, 'START', kind="config", auto_monitor=True)
+    width = Cpt(EpicsSignal, 'WID', kind="config", auto_monitor=True)
+    step = Cpt(EpicsSignal, 'STEP', kind="config", auto_monitor=True)
 
 
 class ZebraPCGate(ZebraPCBase):
-    num_gates = Cpt(EpicsSignal, 'NGATE')
+    num_gates = Cpt(EpicsSignal, 'NGATE', kind="config", auto_monitor=True)
 
 
 class ZebraPCPulse(ZebraPCBase):
-    max = Cpt(EpicsSignal, 'MAX')
-    delay = Cpt(EpicsSignal, 'DLY')
-    status = Cpt(EpicsSignalRO, 'INP.STA')
+    max = Cpt(EpicsSignal, 'MAX', kind="config", auto_monitor=True)
+    delay = Cpt(EpicsSignal, 'DLY', kind="config", auto_monitor=True)
+    status = Cpt(EpicsSignalRO, 'INP.STA', kind="omitted")
 
 
 class ZebraPositionCompare(Device):
-    arm_status = Cpt(EpicsSignalRO, 'ARM_INP.STA')
-    download_count = Cpt(EpicsSignalRO, 'NUM_DOWN')
-    disarm = Cpt(EpicsSignal, 'DISARM')
-    encoder = Cpt(EpicsSignal, 'ENC')
-    enc_x = Cpt(EpicsSignal, 'ENC1')
-    enc_y = Cpt(EpicsSignal, 'ENC2')
-    enc_z = Cpt(EpicsSignal, 'ENC3')
-    enc_omega = Cpt(EpicsSignal, 'ENC4')
-    encoder = Cpt(EpicsSignal, 'ENC')
-    direction = Cpt(EpicsSignal, 'DIR')
-    gate = Cpt(ZebraPCGate, 'GATE_')
+    arm_status = Cpt(EpicsSignalRO, 'ARM_INP.STA', kind="omitted")
+    download_count = Cpt(EpicsSignalRO, 'NUM_DOWN', kind="omitted")
+    disarm = Cpt(EpicsSignal, 'DISARM', kind="omitted")
+    encoder = Cpt(EpicsSignal, 'ENC', kind="config", auto_monitor=True)
+    enc_x = Cpt(EpicsSignal, 'ENC1', kind="omitted")
+    enc_y = Cpt(EpicsSignal, 'ENC2', kind="omitted")
+    enc_z = Cpt(EpicsSignal, 'ENC3', kind="omitted")
+    enc_omega = Cpt(EpicsSignal, 'ENC4', kind="omitted")
+    direction = Cpt(EpicsSignal, 'DIR', kind="config", auto_monitor=True)
+    gate = Cpt(ZebraPCGate, 'GATE_', kind="config", auto_monitor=True)
     pulse = Cpt(ZebraPCPulse, 'PULSE_')
 
 
 class ZebraAnd(Device):
-    inp1 = Cpt(EpicsSignal, 'INP1:STA')
-    inp2 = Cpt(EpicsSignal, 'INP2:STA')
-    out1 = Cpt(EpicsSignal, 'OUT1_TTL')
+    inp1 = Cpt(EpicsSignal, 'INP1:STA', kind="omitted")
+    inp2 = Cpt(EpicsSignal, 'INP2:STA', kind="omitted")
+    out1 = Cpt(EpicsSignal, 'OUT1_TTL', kind="config", auto_monitor=True)
 
 
 class Zebra(Device):
-    downloading = Cpt(EpicsSignal, 'ARRAY_ARQ')
-    reset = Cpt(EpicsSignal, 'SYS_RESET.PROC')
-    m1_set_pos = Cpt(EpicsSignal, 'M1:SETPOS.PROC')
-    m2_set_pos = Cpt(EpicsSignal, 'M2:SETPOS.PROC')
-    m3_set_pos = Cpt(EpicsSignal, 'M3:SETPOS.PROC')
-    m4_set_pos = Cpt(EpicsSignal, 'M4:SETPOS.PROC')
+    downloading = Cpt(EpicsSignal, 'ARRAY_ACQ', kind="omitted")
+    reset = Cpt(EpicsSignal, 'SYS_RESET.PROC', kind="omitted")
+    m1_set_pos = Cpt(EpicsSignal, 'M1:SETPOS.PROC', kind="omitted")
+    m2_set_pos = Cpt(EpicsSignal, 'M2:SETPOS.PROC', kind="omitted")
+    m3_set_pos = Cpt(EpicsSignal, 'M3:SETPOS.PROC', kind="omitted")
+    m4_set_pos = Cpt(EpicsSignal, 'M4:SETPOS.PROC', kind="omitted")
     pc = Cpt(ZebraPositionCompare, 'PC_')
     zebra_and = Cpt(ZebraAnd, 'AND1_')

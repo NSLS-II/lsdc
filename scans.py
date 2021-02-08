1ad3656c5cf371cca747ead8e1ddd7a5d246075a
Jun Aishima <jaishima@bnl.gov>
2021-02-08 16:35:47 -0500

    add function to set up zebra for vector scans

--- scans.py
+++ scans.py
@@ -1,5 +1,6 @@
 #objects available should be zebra, vector, eiger
 import bluesky.plan_stubs as bps
+from start_bs import zebra
 
 def zebraDaqPrep():
        RESET_GROUP = 'reset'
@@ -11,3 +12,15 @@ def zebraDaqPrep():
        yield from bps.mv(zebra.m2_set_pos, 1)
        yield from bps.mv(zebra.m3_set_pos, 1)
        yield from bps.mv(zebra.pc.arm_sel, 1)
+
+def setupZebraVectorScan(angle_start, gate_width, scan_width, pulse_width, pulse_step, exposure_period_per_image, num_images, is_still=False):
+    yield from bps.mv(zebra.pc.gate.sel, angle_start)
+    if is_still is False:
+        yield from bps.mv(zebra.pc.gate.width, gate_width)
+        yield from bps.mv(zebra.pc.gate.step, scan_width)
+    yield from bps.mv(zebra.pc.gate.num_gates, 1)
+    yield from bps.mv(zebra.pc.pulse.start, 0)
+    yield from bps.mv(zebra.pc.pulse.width, pulse_width)
+    yield from bps.mv(zebra.pc.pulse.step, pulse_step)
+    yield from bps.mv(zebra.pc.pulse.delay, exposure_period_per_image/2*1000)
+    yield from bps.mv(zebra.pc.pulse.max, num_images)

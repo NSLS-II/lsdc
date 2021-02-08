23ddb56a29c5c08143fa996a3ad0948d8da617ba
Jun Aishima <jaishima@bnl.gov>
2021-02-08 17:43:21 -0500

    add vector method setup for raster

--- scans.py
+++ scans.py
@@ -24,3 +24,21 @@ def setupZebraVectorScan(angle_start, gate_width, scan_width, pulse_width, pulse
     yield from bps.mv(zebra.pc.pulse.step, pulse_step)
     yield from bps.mv(zebra.pc.pulse.delay, exposure_period_per_image/2*1000)
     yield from bps.mv(zebra.pc.pulse.max, num_images)
+
+def setupZebraVectorScanForRaster(angle_start, image_width, exposure_time_per_image, exposure_period_per_image, detector_dead_time, num_images, scan_encoder=3):
+    VECTOR_SCAN_GROUP = 'vector_scan'
+    yield from bps.abs_set(zebra.pc.encoder, scan_encoder, group=VECTOR_SCAN_GROUP)
+    yield from bps.wait(VECTOR_SCAN_GROUP)
+    yield from bps.sleep(1.0)
+    yield from bps.mv(zebra.pc.direction, 0) # 0 = positive
+    yield from bps.mv(zebra.pc.gate.sel, 0)
+    yield from bps.mv(zebra.pc.gate.start, angle_start)
+    if image_width != 0:
+        yield from bps.mv(zebra.pc.gate.width, num_images * image_width)
+        yield from bps.mv(zebra.pc.gate.step, num_images * image_width + 0.001)
+    yield from bps.mv(zebra.pc.gate.num_gates, 1)
+    yield from bps.mv(zebra.pc.pulse.sel, 1)
+    yield from bps.mv(zebra.pc.pulse.start, 0)
+    yield from bps.mv(zebra.pc.pulse.width, (exposure_time_per_image - detector_dead_time) * 1000)
+    yield from bps.mv(zebra.pc.pulse.step, exposure_period_per_image * 1000)
+    yield from bps.mv(zebra.pc.pulse.delay, exposure_period_per_image / 2 * 1000)

f7c6dfe788f266044045dc1c41087d9127707776
Jun Aishima <jaishima@bnl.gov>
2021-02-17 15:48:42 -0500

    move RE() to around zebraDaq()
     * moved to the highest level that makes sense with the LSDC
       hierarchy to prevent issues of nested RE() calls and generally
       follow good practice
     * the focus is on the standard oscillation experiment, so
       RE is moved to wrap zebraDaq() in collect_detector_seq_hw

--- daq_macros.py
+++ daq_macros.py
@@ -2984,7 +2984,7 @@ def zebraCamDaq(angle_start,scanWidth,imgWidth,exposurePeriodPerImage,filePrefix
   setPvDesc("vectorNumFrames",numImages)    
   setPvDesc("vectorframeExptime",exposurePeriodPerImage*1000.0)
   setPvDesc("vectorHold",0)
-  RE(zebra_daq_prep())
+  yield from zebra_daq_prep()
   setPvDesc("zebraEncoder",scanEncoder)
   setPvDesc("zebraDirection",0)  #direction 0 = positive
   setPvDesc("zebraGateSelect",0)
@@ -3070,7 +3070,7 @@ def zebraDaq(angle_start,scanWidth,imgWidth,exposurePeriodPerImage,filePrefix,da
   setPvDesc("vectorframeExptime",exposurePeriodPerImage*1000.0)
   setPvDesc("vectorHold",0)
   logger.info("zebra_daq_prep " + str(time.time()))        
-  RE(zebra_daq_prep())
+  yield from zebra_daq_prep()
   logger.info("done zebra_daq_prep " + str(time.time()))        
   time.sleep(1.0)
 
@@ -3078,8 +3078,8 @@ def zebraDaq(angle_start,scanWidth,imgWidth,exposurePeriodPerImage,filePrefix,da
   PW=(exposurePeriodPerImage-detector_dead_time)*1000.0
   PS=(exposurePeriodPerImage)*1000.0
   GW=scanWidth-(1.0-(PW/PS))*(imgWidth/2.0)
-  RE(setup_zebra_vector_scan(angle_start=angle_start, gate_width=GW, scan_width=scanWidth, pulse_width=PW, pulse_step=PS,
-                       exposure_period_per_image=exposurePeriodPerImage, num_images=numImages, is_still=imgWidth==0))
+  yield from setup_zebra_vector_scan(angle_start=angle_start, gate_width=GW, scan_width=scanWidth, pulse_width=PW, pulse_step=PS,
+                       exposure_period_per_image=exposurePeriodPerImage, num_images=numImages, is_still=imgWidth==0)
   logger.info("zebraDaq - setting and arming detector " + str(time.time()))      
 
   det_lib.detector_set_num_triggers(1)  
@@ -3136,7 +3136,7 @@ def zebraDaqNoDet(angle_start,scanWidth,imgWidth,exposurePeriodPerImage,filePref
   setPvDesc("vectorEndOmega",angle_end)
   setPvDesc("vectorframeExptime",exposurePeriodPerImage*1000.0)
   setPvDesc("vectorHold",0)
-  RE(zebra_daq_prep())
+  yield from zebra_daq_prep()
   setPvDesc("zebraEncoder",scanEncoder)
   time.sleep(1.0)
   setPvDesc("zebraDirection",0)  #direction 0 = positive
@@ -3197,11 +3197,11 @@ def zebraVecDaqSetup(angle_start,imgWidth,exposurePeriodPerImage,numImages,fileP
   detector_dead_time = det_lib.detector_get_deadtime()
   total_exposure_time = exposurePeriodPerImage*numImages
   exposureTimePerImage =  exposurePeriodPerImage - detector_dead_time
-  RE(zebra_daq_prep())
+  yield from zebra_daq_prep()
 
-  RE(setup_zebra_vector_scan_for_raster(angle_start=angle_start, image_width=imgWidth, exposure_time_per_image=exposureTimePerImage,
+  yield from setup_zebra_vector_scan_for_raster(angle_start=angle_start, image_width=imgWidth, exposure_time_per_image=exposureTimePerImage,
                                 exposure_period_per_image=exposurePeriodPerImage, detector_dead_time=detector_dead_time,
-                                num_images=numImages, scan_encoder=scan_encoder))
+                                num_images=numImages, scan_encoder=scan_encoder)
   logger.info("exp tim = " + str(exposureTimePerImage))  
 
   setPvDesc("vectorHold",0)  

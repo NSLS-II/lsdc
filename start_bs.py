3890620c89f66cd7970b3926c8692e573e4354b6
Jun Aishima <jaishima@bnl.gov>
2021-06-23 15:32:41 -0400

    extract vector_program into mxtools

--- start_bs.py
+++ start_bs.py
@@ -4,6 +4,7 @@ from ophyd import *
 from ophyd.mca import (Mercury1, SoftDXPTrigger)
 from ophyd import Device, EpicsMotor, EpicsSignal, EpicsSignalRO
 from zebra import Zebra
+from vector_program import VectorProgram, VectorProgramStart, VectorProgramEnd
 import os
 #12/19 - author unknown. DAMA can help
 """
@@ -85,40 +86,6 @@ class VerticalDCM(Device):
     r = Cpt(EpicsMotor, '-Ax:R}Mtr')
     e = Cpt(EpicsMotor, '-Ax:E}Mtr')
     w = Cpt(EpicsMotor, '-Ax:W}Mtr')
-
-
-class VectorProgramStart(Device):
-    omega = Cpt(EpicsSignal, 'Pos:OStart-SP')
-    x = Cpt(EpicsSignal, 'Pos:XStart-SP')
-    y = Cpt(EpicsSignal, 'Pos:YStart-SP')
-    z = Cpt(EpicsSignal, 'Pos:ZStart-SP')
-
-
-class VectorProgramEnd(Device):
-    omega = Cpt(EpicsSignal, 'Pos:OEnd-SP')
-    x = Cpt(EpicsSignal, 'Pos:XEnd-SP')
-    y = Cpt(EpicsSignal, 'Pos:YEnd-SP')
-    z = Cpt(EpicsSignal, 'Pos:ZEnd-SP')
-
-
-class VectorProgram(Device):
-    start = Cpt(VectorProgramStart, '')
-    end = Cpt(VectorProgramEnd, '')
-
-    abort = Cpt(EpicsSignal, 'Cmd:Abort-Cmd', kind='omitted')
-    go = Cpt(EpicsSignal, 'Cmd:Go-Cmd', kind='omitted')
-    proceed = Cpt(EpicsSignal, 'Cmd:Proceed-Cmd', kind='omitted')
-    sync = Cpt(EpicsSignal, 'Cmd:Sync-Cmd', kind='omitted')
-
-    expose = Cpt(EpicsSignal, 'Expose-Sel')
-    hold = Cpt(EpicsSignal, 'Hold-Sel')
-
-    buffer_time = Cpt(EpicsSignal, 'Val:BufferTime-SP')
-    frame_exptime = Cpt(EpicsSignal, 'Val:Exposure-SP')
-    num_frames = Cpt(EpicsSignal, 'Val:NumSamples-SP')
-
-    active = Cpt(EpicsSignalRO, 'Sts:Running-Sts')
-    state = Cpt(EpicsSignalRO, 'Sts:State-Sts')
 
 
 class StandardProsilica(SingleTrigger, ProsilicaDetector):

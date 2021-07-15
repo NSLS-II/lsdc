2285c7e24f351919cc10a1af4027b336e2e7b9eb
Jun Aishima <jaishima@bnl.gov>
2021-07-15 11:37:21 -0400

    update to use new PV

--- lsdcGui.py
+++ lsdcGui.py
@@ -4634,7 +4634,7 @@ class ControlMain(QtWidgets.QMainWindow):
         self.timerHutch.start(HUTCH_TIMER_DELAY)      
         if reply == QtWidgets.QMessageBox.Yes:
           if daq_utils.beamline == "fmx" or daq_utils.beamline == 'amx':
-            os.system("ssh -q -X " + getBlConfig('hostnameBase') + "-lsdcserver \"cd " + os.getcwd() + ";xterm -e /usr/local/bin/lsdcServer\"&")
+            setPvDesc('restartServer', (int(getPvDesc('restartServer')) + 1) % 100)
           else:
             logger.error('Not restarting server - unknown beamline')
       else:

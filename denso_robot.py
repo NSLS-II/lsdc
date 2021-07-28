1fc673ad943b6f877efbd45920153b4d2f175863
Jun Aishima <jaishima@bnl.gov>
2021-07-28 13:05:14 -0400

    add interface class, specify types in function parameters

--- denso_robot.py
+++ denso_robot.py
@@ -2,14 +2,19 @@ class DensoRobot:
     def __init__(self, robot):
         self.robot = robot
 
-    def preMount(self):
+    def preMount(self, puck_pos: int, pin_pos: int, samp_id: str):
         ...
 
-    def mount(self):
+    def mount(self, puck_pos: int, pin_pos: int, samp_id: str, **kwargs):
+        try:
+            self.robot.mount(get_puck_letter(puck_pos), str(pin_pos))
+        except Exception as e:
+            logger.error(f'Exception during mount step: {e}')
+            return MOUNT_FAILURE
+        return MOUNT_STEP_SUCCESSFUL
+
+    def postMount(self, puck_pos: int, pin_pos: int, samp_id: str):
         ...
 
-    def postMount(self):
-        ...
-
-    def unmount(self):
-        ...
+    def unmount(self, puck_pos: int, pin_pos: int, abs_pos=0):
+        self.robot.dismount(str(puck_pos), str(pin_pos))

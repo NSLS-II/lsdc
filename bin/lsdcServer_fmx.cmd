61ca402d6861dc29d74cf2e190d4bdbb3768fc64
Jun Aishima <jaishima@bnl.gov>
2021-09-03 12:12:22 -0400

    switch over to using xf17id2 location of LSDC code and configs
     * the actual change was last cycle, but we have had the
       convenience symlink from /GPFS/CENTRAL/xf17id1/skinnerProjectsBackup
       to xf17id2

--- bin/lsdcServer_fmx.cmd
+++ bin/lsdcServer_fmx.cmd
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/
+export PROJDIR=/GPFS/CENTRAL/xf17id2/skinnerProjectsBackup/
 export CONFIGDIR=${PROJDIR}bnlpx_config/
 export LSDCHOME=${PROJDIR}lsdc
 export PYTHONPATH=".:${CONFIGDIR}:/usr/local/edna-mx/mxv1/src:/usr/local/edna-mx/kernel/src:${LSDCHOME}:${PROJDIR}/RobotControlMerge"
-export PATH=/usr/local/bin:/usr/bin:/bin:${PROJDIR}/software/bin:/GPFS/CENTRAL/XF17ID1/usr/local/crys/ccp4-6.5/bin
+export PATH=/usr/local/bin:/usr/bin:/bin:${PROJDIR}/software/bin:/GPFS/CENTRAL/XF17ID2/usr/local/crys/ccp4-6.5/bin
 source ${CONFIGDIR}daq_env.txt
 export matlab_distrib=${PROJDIR}/software/c3d/matlab_distrib
 export LD_LIBRARY_PATH=$matlab_distrib/bin/glnx86:$matlab_distrib/toolbox

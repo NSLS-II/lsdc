05c9667fdb9434342dbe5e5ef057bdb0180b8458
Jun Aishima <jaishima@bnl.gov>
2021-09-07 15:43:37 -0400

    fix wrong energy in HDF5 files
     * energy2wave only used 2 digits for wavelength, the resulting
       rounding caused energy to be inaccurate
     * added the option to define more digits after the decimal for
       both energy2wave and wave2energy
     * use 6 digits after the decimal for this conversion

--- daq_utils.py
+++ daq_utils.py
@@ -111,17 +111,17 @@ def distance_from_reso(det_radius,reso,wave,theta):
     return 501.0 #a safe value for now
 
 
-def energy2wave(e):
+def energy2wave(e, digits=2):
   if (float(e)==0.0):
     return 1.0
   else:
-    return float("%.2f" % (EV_ANGSTROM_CONSTANT/e))
+    return float(f"%.{digits}f" % (EV_ANGSTROM_CONSTANT/e))
 
-def wave2energy(w):
+def wave2energy(w, digits=2):
   if (float(w)==0.0):
     return 12600.0
   else:
-    return float("%.2f" % (EV_ANGSTROM_CONSTANT/w))
+    return float(f"%.{digits}f" % (EV_ANGSTROM_CONSTANT/w))
 
 def createDefaultRequest(sample_id,createVisit=True):
     """

f9392688aa54e63f72ce4ad54d2b4308895ce6bb
Jun Aishima <jaishima@bnl.gov>
2021-10-08 14:17:21 -0400

    add wait keyword, DI move can also use this

--- gov_lib.py
+++ gov_lib.py
@@ -15,15 +15,19 @@ def set_detz_out(distance):
     gov_human.dev.dz.target_Out.set(distance)
     gov_robot.dev.dz.target_Out.set(distance)
 
-def setGovRobot(state):
+def setGovRobot(state, wait=True):
   try:
     logger.info(f"setGovRobot{state}")
     govStatus = gov_robot.set(state)
-    waitGov(govStatus)
-    if state in ["SA", "DA"]:
+    if not wait:
+        return govStatus
+    failure = waitGov(govStatus)
+    if not failure and (state in ["SA", "DA", "DI"]):
       toggleLowMagCameraSettings(state)
+    return not failure
   except Exception: #TODO verify what kind of exception is thrown if we do not reach state
     logger.info(f"Governor did not reach {state}")
+    return 0
 
 def waitGov(status):
   try:

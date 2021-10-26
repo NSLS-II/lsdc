e2f9675c371173abb5eb0e9c251c3553f220569b
Jun Aishima <jaishima@bnl.gov>
2021-10-26 15:42:04 -0400

    do not move detector for fluorescence scans
     * eScan and specRaster
     * #153

--- daq_lib.py
+++ daq_lib.py
@@ -593,8 +593,9 @@ def collectData(currentRequest):
     os.system(comm_s)
   logger.debug('starting initial motions - transmission and detector distance')
   daq_macros.setTrans(attenuation)
-  beamline_lib.mvaDescriptor("detectorDist",colDist)  
-  logger.debug('transmission and detector distance done')
+  if prot not in ["rasterScreen", "eScan"]:
+    beamline_lib.mvaDescriptor("detectorDist",colDist)  
+  logger.debug('transmission and detector distance (if not fluorescence-related) done')
   # now that the detector is in the correct position, get the beam center
   currentRequest['request_obj']['xbeam'] = getPvDesc('beamCenterX')
   currentRequest['request_obj']['ybeam'] = getPvDesc('beamCenterY')

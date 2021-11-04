ec2c686a494d20dcc2f56cb8d7e43844044e7fdc
Jun Aishima <jaishima@bnl.gov>
2021-11-04 18:30:40 -0400

    use run engine for robot commands

--- denso_robot.py
+++ denso_robot.py
@@ -3,6 +3,7 @@ from daq_utils import getBlConfig
 from config_params import MOUNT_SUCCESSFUL, MOUNT_STEP_SUCCESSFUL, MOUNT_FAILURE,\
                           UNMOUNT_SUCCESSFUL, UNMOUNT_FAILURE
 import gov_lib
+from start_bs import RE
 import logging
 logger = logging.getLogger(__name__)
 
@@ -25,7 +26,7 @@ class DensoRobot:
 
     def mount(self, puck_pos: int, pin_pos: int, samp_id: str, **kwargs):
         try:
-            self.robot.mount(get_puck_letter(puck_pos), str(pin_pos))
+            RE(self.robot.mount(get_puck_letter(puck_pos), str(pin_pos)))
         except Exception as e:
             logger.error(f'Exception during mount step: {e}')
             return MOUNT_FAILURE
@@ -52,7 +53,7 @@ class DensoRobot:
 
     def unmount(self, puck_pos: int, pin_pos: int, samp_id: str):
         try:
-            self.robot.dismount(get_puck_letter(puck_pos), str(pin_pos))
+            RE(self.robot.dismount(get_puck_letter(puck_pos), str(pin_pos)))
         except Exception as e:
             logger.error(f'Exception while unmounting sample: {e}')
             return UNMOUNT_FAILURE

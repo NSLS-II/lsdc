b4e424ace7e94ac3b8ebd36cb8705dec43d6b04f
Jun Aishima <jaishima@bnl.gov>
2021-11-22 11:39:37 -0500

    use correct function name

--- daq_macros.py
+++ daq_macros.py
@@ -3113,7 +3113,7 @@ def zebraDaqBluesky(flyer, angle_start, scanWidth, imgWidth, exposurePeriodPerIm
         current_z *= 1000
         det_distance_m /= 1000  # convert distance from mm to m on NYX
 
-    flyer.update_params(angle_start=angle_start, scan_width=scanWidth, img_width=imgWidth, num_images=num_images, exposure_period_per_image=exposurePeriodPerImage, \
+    flyer.update_parameters(angle_start=angle_start, scan_width=scanWidth, img_width=imgWidth, num_images=num_images, exposure_period_per_image=exposurePeriodPerImage, \
                    x_start_um=current_x, y_start_um=current_y, z_start_um=current_z, \
                    file_prefix=filePrefix, data_directory_name=dataDirectoryName, file_number_start=file_number_start,\
                    x_beam=x_beam, y_beam=y_beam, wavelength=wavelength, det_distance_m=det_distance_m,\

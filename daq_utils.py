5662965d381e4e6fdfd9637e2a4baa53fd84dafb
Michael Skinner <mskinner@bnl.gov>
2022-01-31 10:47:25 -0800

    Clarified description comments reflecting camera position and orientation for each of the CAMERA_ANGLE_ constants

--- daq_utils.py
+++ daq_utils.py
@@ -29,9 +29,9 @@ global screenYCenterPixelsLowMagOffset
 screenYCenterPixelsLowMagOffset = 58
 # Constants for use with C2C
 global CAMERA_ANGLE_BEAM,CAMERA_ANGLE_ABOVE, CAMERA_ANGLE_BELOW
-CAMERA_ANGLE_BEAM = 0 # Camera is in line with beam
-CAMERA_ANGLE_ABOVE = 1 # Camera is above sample looking down
-CAMERA_ANGLE_BELOW = 2 # Camera is below sample looking up
+CAMERA_ANGLE_BEAM = 0 # Camera is in line with beam, upstream from the sample, facing downstream, top toward ceiling
+CAMERA_ANGLE_ABOVE = 1 # Camera is directly above sample facing downward, top of camera is facing downstream
+CAMERA_ANGLE_BELOW = 2 # Camera is directly below sample facing upward, top of camera is facing downstream
 
 def getBlConfig(param, beamline=beamline):
         return db_lib.getBeamlineConfigParam(beamline, param)

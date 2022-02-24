7d11bc9c68c2e7c5c7bd74f7551fe43ca37193e3
Michael Skinner <m.skinner5278@gmail.com>
2022-02-24 14:36:10 -0500

    Added QIntValidator with a range of 1-99999 for filenum start field

--- lsdcGui.py
+++ lsdcGui.py
@@ -1422,6 +1422,7 @@ class DataLocInfo(QtWidgets.QGroupBox):
         self.hBoxDPathParams2.addWidget(self.prefix_ledit)
         self.dataNumstartLabel = QtWidgets.QLabel('File Number Start:')
         self.file_numstart_ledit = QtWidgets.QLineEdit()
+        self.file_numstart_ledit.setValidator(QIntValidator(1, 99999, self))
         self.file_numstart_ledit.setFixedWidth(50)
         self.hBoxDPathParams3 = QtWidgets.QHBoxLayout()
         self.dataPathLabel = QtWidgets.QLabel('Data Path:')

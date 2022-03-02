0246f75635efd480c659732c3771401ad0454da8
Michael Skinner <mskinner@bnl.gov>
2022-03-02 08:32:57 -0800

    Added imageScale and imageScaleText recoloring to settings menu, refactor Crosshair to Overlay in settings menu. Refactoring is necessary to clarify that the color changes will be applied to the entire image overlay and not just the crosshair element.

--- lsdcGui.py
+++ lsdcGui.py
@@ -5197,55 +5197,65 @@ class ControlMain(QtWidgets.QMainWindow):
         fileMenu.addAction(self.userAction)
         fileMenu.addAction(self.expertAction)
         fileMenu.addAction(self.staffAction)                
-        # Define all of the available actions for the crosshair color group
-        self.BlueCrosshairAction = QtWidgets.QAction('Blue', self, checkable=True)
-        self.RedCrosshairAction = QtWidgets.QAction('Red', self, checkable=True)
-        self.GreenCrosshairAction = QtWidgets.QAction('Green', self, checkable=True)
-        self.WhiteCrosshairAction = QtWidgets.QAction('White', self, checkable=True)
-        self.BlackCrosshairAction = QtWidgets.QAction('Black', self, checkable=True)
+        # Define all of the available actions for the overlay color group
+        self.BlueOverlayAction = QtWidgets.QAction('Blue', self, checkable=True)
+        self.RedOverlayAction = QtWidgets.QAction('Red', self, checkable=True)
+        self.GreenOverlayAction = QtWidgets.QAction('Green', self, checkable=True)
+        self.WhiteOverlayAction = QtWidgets.QAction('White', self, checkable=True)
+        self.BlackOverlayAction = QtWidgets.QAction('Black', self, checkable=True)
         # Connect all of the trigger callbacks to their respective actions
-        self.BlueCrosshairAction.triggered.connect(self.blueCrosshairTriggeredCB)
-        self.RedCrosshairAction.triggered.connect(self.redCrosshairTriggeredCB)
-        self.GreenCrosshairAction.triggered.connect(self.greenCrosshairTriggeredCB)
-        self.WhiteCrosshairAction.triggered.connect(self.whiteCrosshairTriggeredCB)
-        self.BlackCrosshairAction.triggered.connect(self.blackCrosshairTriggeredCB)
+        self.BlueOverlayAction.triggered.connect(self.blueOverlayTriggeredCB)
+        self.RedOverlayAction.triggered.connect(self.redOverlayTriggeredCB)
+        self.GreenOverlayAction.triggered.connect(self.greenOverlayTriggeredCB)
+        self.WhiteOverlayAction.triggered.connect(self.whiteOverlayTriggeredCB)
+        self.BlackOverlayAction.triggered.connect(self.blackOverlayTriggeredCB)
         # Create the action group and populate it
-        self.centerMarkerColorActionGroup = QtWidgets.QActionGroup(self)
-        self.centerMarkerColorActionGroup.setExclusive(True)
-        self.centerMarkerColorActionGroup.addAction(self.BlueCrosshairAction)
-        self.centerMarkerColorActionGroup.addAction(self.RedCrosshairAction)
-        self.centerMarkerColorActionGroup.addAction(self.GreenCrosshairAction)
-        self.centerMarkerColorActionGroup.addAction(self.WhiteCrosshairAction)
-        self.centerMarkerColorActionGroup.addAction(self.BlackCrosshairAction)
+        self.overlayColorActionGroup = QtWidgets.QActionGroup(self)
+        self.overlayColorActionGroup.setExclusive(True)
+        self.overlayColorActionGroup.addAction(self.BlueOverlayAction)
+        self.overlayColorActionGroup.addAction(self.RedOverlayAction)
+        self.overlayColorActionGroup.addAction(self.GreenOverlayAction)
+        self.overlayColorActionGroup.addAction(self.WhiteOverlayAction)
+        self.overlayColorActionGroup.addAction(self.BlackOverlayAction)
         # Create the menu item with the submenu, add the group 
-        self.centerMarkerMenu = settingsMenu.addMenu("Crosshair Settings")
-        self.centerMarkerMenu.addActions(self.centerMarkerColorActionGroup)
-        self.BlueCrosshairAction.setChecked(True)
+        self.overlayMenu = settingsMenu.addMenu("Overlay Settings")
+        self.overlayMenu.addActions(self.overlayColorActionGroup)
+        self.BlueOverlayAction.setChecked(True)
         
         fileMenu.addAction(exitAction)
         self.setGeometry(300, 300, 1550, 1000) #width and height here. 
         self.setWindowTitle('LSDC on %s' % daq_utils.beamline)
         self.show()
 
-    def blueCrosshairTriggeredCB(self):
-        centerMarkBrush = QtGui.QBrush(QtCore.Qt.blue)                
-        self.centerMarker.setBrush(centerMarkBrush)
+    def blueOverlayTriggeredCB(self):
+        overlayBrush = QtGui.QBrush(QtCore.Qt.blue)                
+        self.centerMarker.setBrush(overlayBrush)
+        self.imageScale.setBrush(overlayBrush)
+        self.imageScaleText.setBrush(overlayBrush)
 
-    def redCrosshairTriggeredCB(self):
-        centerMarkBrush = QtGui.QBrush(QtCore.Qt.red)                
-        self.centerMarker.setBrush(centerMarkBrush)
+    def redOverlayTriggeredCB(self):
+        overlayBrush = QtGui.QBrush(QtCore.Qt.red)                
+        self.centerMarker.setBrush(overlayBrush)
+        self.imageScale.setBrush(overlayBrush)
+        self.imageScaleText.setBrush(overlayBrush)
 
-    def greenCrosshairTriggeredCB(self):
-        centerMarkBrush = QtGui.QBrush(QtCore.Qt.green)                
-        self.centerMarker.setBrush(centerMarkBrush)
+    def greenOverlayTriggeredCB(self):
+        overlayBrush = QtGui.QBrush(QtCore.Qt.green)                
+        self.centerMarker.setBrush(overlayBrush)
+        self.imageScale.setBrush(overlayBrush)
+        self.imageScaleText.setBrush(overlayBrush)
 
-    def whiteCrosshairTriggeredCB(self):
-        centerMarkBrush = QtGui.QBrush(QtCore.Qt.white)                
-        self.centerMarker.setBrush(centerMarkBrush)
+    def whiteOverlayTriggeredCB(self):
+        overlayBrush = QtGui.QBrush(QtCore.Qt.white)                
+        self.centerMarker.setBrush(overlayBrush)
+        self.imageScale.setBrush(overlayBrush)
+        self.imageScaleText.setBrush(overlayBrush)
 
-    def blackCrosshairTriggeredCB(self):
-        centerMarkBrush = QtGui.QBrush(QtCore.Qt.black)                
-        self.centerMarker.setBrush(centerMarkBrush)
+    def blackOverlayTriggeredCB(self):
+        overlayBrush = QtGui.QBrush(QtCore.Qt.black)                
+        self.centerMarker.setBrush(overlayBrush)
+        self.imageScale.setBrush(overlayBrush)
+        self.imageScaleText.setBrush(overlayBrush)
 
     def popStaffDialogCB(self):
       if (self.controlEnabled()):

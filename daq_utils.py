58e0d2c9d434e8032e316d12074b37f2a0f07e35
Michael Skinner <mskinner@bnl.gov>
2022-06-30 06:38:39 -0700

    Every reference to beamCheck beamline config now uses config_param string

--- daq_utils.py
+++ daq_utils.py
@@ -1,4 +1,5 @@
 import os
+from config_params import BEAM_CHECK
 from math import *
 import math
 import requests
@@ -84,7 +85,7 @@ def init_environment():
   varname = "DETECTOR_OFFLINE"
   if varname in os.environ:
     detectorOffline = int(os.environ[varname])
-  setBlConfig("beamCheck",1)
+  setBlConfig(BEAM_CHECK,1)
 
 
 def calc_reso(det_radius,detDistance,wave,theta):

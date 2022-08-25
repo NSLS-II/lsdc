7b71c1a85818ee24492595c9ccd97bcf0cf94e7a
Jun Aishima <jaishima@bnl.gov>
2022-08-25 16:13:13 -0400

    update to lsdc-server-2022-3.1 conda env

--- bin/lsdcServer_fmx.cmd
+++ bin/lsdcServer_fmx.cmd
@@ -2,7 +2,7 @@
 export PROJDIR=/nsls2/software/mx/daq/
 export CONFIGDIR=${PROJDIR}bnlpx_config/
 export LSDCHOME=${PROJDIR}lsdc_fmx
-export PYTHONPATH=".:${CONFIGDIR}:/usr/lib64/edna-mx/mxv1/src:/usr/lib64/edna-mx/kernel/src:${LSDCHOME}:${PROJDIR}/RobotControlLib"
+export PYTHONPATH="/nsls2/data/fmx/shared/config/lsdc_overlay/lsdc-server-2022-3.1/lib/python3.8/site-packages:.:${CONFIGDIR}:/usr/lib64/edna-mx/mxv1/src:/usr/lib64/edna-mx/kernel/src:${LSDCHOME}:${PROJDIR}/RobotControlLib"
 export PATH=/usr/local/bin:/usr/bin:/bin:${PROJDIR}/software/bin:/opt/ccp4/bin
 source ${CONFIGDIR}daq_env.txt
 export matlab_distrib=${PROJDIR}/software/c3d/matlab_distrib
@@ -10,5 +10,5 @@ export LD_LIBRARY_PATH=$matlab_distrib/bin/glnx86:$matlab_distrib/toolbox
 export PINALIGNDIR=${PROJDIR}pinAlign/pin_align-master/
 export MXPROCESSINGSCRIPTSDIR=${PROJDIR}lsdc-processing/
 # below not idea as environment name also needed by daq_main2
-conda activate lsdc-server-2022-2.3
+conda activate lsdc-server-2022-3.1
 $LSDCHOME/lsdcServer

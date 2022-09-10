60d0a2ec55e274fd20d7dc6eae134805d94d36de
vshekar <vshekar1@bnl.gov>
2022-09-10 14:49:15 -0400

    Pucks list sorted in desc order of modification
    Whenever a new puck is created or an existing puck is updated, it creates/updates a field called "modified_time" to the current time
    In the GUI these pucks are ordered in descending order of modified_time
    If no modified_time exists, it's modified_time is considered 0 for sort

--- lsdcGui.py
+++ lsdcGui.py
@@ -993,66 +993,66 @@ class PuckDialog(QtWidgets.QDialog):
         puckListUnsorted = db_lib.getAllPucks(daq_utils.owner)
         puckList = sorted(puckListUnsorted,key=lambda i: i['name'],reverse=False)
         dewarObj = db_lib.getPrimaryDewar(daq_utils.beamline)
-        pucksInDewar = dewarObj['content']
-        data = []
-#if you have to, you could store the puck_id in the item data
-        for i in range(len(puckList)):
-          if (puckList[i]["uid"] not in pucksInDewar):
-            data.append(puckList[i]["name"])
-        self.model = QtGui.QStandardItemModel()
+        pucksInDewar = set(dewarObj['content'])
+        self.model = QtGui.QStandardItemModel(self)
+        self.proxyModel = QtCore.QSortFilterProxyModel(self)
         labels = QStringList(("Name"))
         self.model.setHorizontalHeaderLabels(labels)
-        for i in range(len(data)):
-            name = QtGui.QStandardItem(data[i])
-            self.model.appendRow(name)
+        for puck in puckList:
+          if puck['uid'] not in pucksInDewar:
+            item = QtGui.QStandardItem(puck["name"])
+            # Adding meta data to the puck. Each piece of meta data is identified using 
+            # an int value, in this case is Qt.UserRole for puck modified time. This metadata is used
+            # to sort pucks
+            item.setData(puck.get('modified_time',0), Qt.UserRole)
+            self.model.appendRow(item)
 
 
     def initUI(self):
         self.tv = QtWidgets.QListView(self)
-        self.tv.setModel(self.model)
+        
         self.tv.doubleClicked[QModelIndex].connect(self.containerOKCB)
         behavior = QtWidgets.QAbstractItemView.SelectRows
         self.tv.setSelectionBehavior(behavior)
-        
+        self.proxyModel.setSourceModel(self.model)
+        self.proxyModel.setSortRole(Qt.UserRole)
+        self.proxyModel.sort(0, order=Qt.DescendingOrder)
+        self.tv.setModel(self.proxyModel)
         self.label = QtWidgets.QLabel(self)
         self.buttons = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
             Qt.Horizontal, self)
         self.buttons.buttons()[0].clicked.connect(self.containerOKCB)
         self.buttons.buttons()[1].clicked.connect(self.containerCancelCB)
+        self.searchBox = QtWidgets.QLineEdit(self)
+        self.searchBox.setPlaceholderText('Filter pucks...')
+        self.searchBox.textChanged.connect(self.filterPucks)
         layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.searchBox)
         layout.addWidget(self.tv) 
         layout.addWidget(self.label)
         layout.addWidget(self.buttons)
-        self.setLayout(layout)        
-        self.tv.clicked.connect(self.onClicked)
+        self.setLayout(layout)
+
+    def filterPucks(self, a0: str):
+      self.proxyModel.setFilterFixedString(a0)       
             
     def containerOKCB(self):
-      selmod = self.tv.selectionModel()
-      selection = selmod.selection()
-      indexes = selection.indexes()
-      if (indexes != []):
-        i = 0
-        item = self.model.itemFromIndex(indexes[i])
-        text = str(item.text())
+      indexes = self.tv.selectedIndexes()
+      if indexes:
+        text = indexes[0].data()
         self.label.setText(text)      
-        self.accept()
         self.puckName = text
+        self.accept()
       else:
         text = ""
-        self.reject()
         self.puckName = text
-      
+        self.reject()
 
     def containerCancelCB(self):
       text = ""
       self.reject()
       self.puckName = text
-
-        
-    def onClicked(self, idx):
-      item = self.model.itemFromIndex(idx)        
-      text = str(item.text())
 
     @staticmethod
     def getPuckName(parent = None):

c24190048a127cb4fa2ce30951b778a7c3a1d5dd
Jun Aishima <jaishima@bnl.gov>
2023-03-07 11:29:31 -0500

    improve handling of sample info
     * prevent items with empty sample names from being shown
       as being in the queue

--- db_lib.py
+++ db_lib.py
@@ -510,15 +510,26 @@ def getQueue(beamlineName):
     for item in DewarItems:
       if (item != ""):
         items.append(item)
+    logger.debug(f"dewar items: {items}")
 
     sample_list = []
     contents = [getContainerByID(uid)['content'] for uid in items]
+    logger.debug(f"total contents: {contents}")
+    for samples in contents:
+        logger.debug(f"samples: {samples}")
+        for samp in samples:
+            logger.debug(f'{samp} {samp!=""}')
+            if (samp != ""):
+              sample_list.append(samp)
+
+    logger.debug(f"sample list: {sample_list}")
     for samp in contents:
         if (samp != ""):
           sample_list += samp
 
     for s in sample_list:
         reqs = getRequestsBySampleID(s, active_only=True)
+        logger.info(f"requests for sample {s}: {reqs}")
         for request in reqs:
             yield request
 

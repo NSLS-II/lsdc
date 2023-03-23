bfe2103572327bacdab43494f4a0fe8bde6644c2
Jun Aishima <jaishima@bnl.gov>
2023-03-23 09:53:34 -0400

    check that an lsdc service user is running the script
     * ensure that a normal user isn't starting up LSDC

--- daq_main2.py
+++ daq_main2.py
@@ -12,6 +12,7 @@ from daq_lib import *
 from robot_lib import *
 from beamline_lib import *
 from gov_lib import setGovRobot
+import getpass
 
 import logging
 from logging import handlers
@@ -27,6 +28,13 @@ handler2.setFormatter(myformat)
 logger.addHandler(handler1)
 logger.addHandler(handler2)
 
+if not getpass.getuser().startswith("lsdc-"):
+    message = "LSDC server not being started by a LSDC service user account, aborting!"
+    print(message)
+    logger.error(message)
+    sys.exit(1)
+else:
+    print(f"continuing as we are using a service user: {getpass.getuser()}")
 sitefilename = ""
 global command_list,immediate_command_list,z
 command_list = []

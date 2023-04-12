58816be62420aa5fe2acacc2abf51f291c6b75ee
Jun Aishima <jaishima@bnl.gov>
2023-04-12 11:21:42 -0400

    Check data before running fastdp (#281)
    * Checks whether master file exists at all
    * Enabled validation of HDF5 before running fastDP

--- albulaUtils.py
+++ albulaUtils.py
@@ -108,6 +108,8 @@ def validate_master_HDF5_file(filename):
   Validate master HDF5 by checking if data files exist and can be read
   """
   path = Path(filename)
+  if not path.exists():
+    return False
   try:
     if 'master' in path.stem and path.suffix == '.h5':
       with h5py.File(path) as f:

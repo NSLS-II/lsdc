4c8c30d998a9e4edec0bc11a05ad55c0e8e3f460
Jun Aishima <jaishima@bnl.gov>
2023-06-29 11:16:29 -0400

    use subprocess.Popen which has the cwd kwarg
     * os.popen does not

--- top_view.py
+++ top_view.py
@@ -6,6 +6,7 @@ from beamline_support import getPvValFromDescriptor as getPvDesc, setPvValFromDe
 import os
 import filecmp
 import logging
+import subprocess
 from config_params import TOP_VIEW_CHECK
 
 logger = logging.getLogger(__name__)
@@ -37,7 +38,7 @@ def wait90TopviewThread(gov_robot, prefix1,prefix90):
     if (not filecmp.cmp(getBlConfig("visitDirectory")+"/pinAlign/"+snapshot1Name,getBlConfig("visitDirectory")+"/pinAlign/"+snapshot2Name)): #this would mean something is wrong if true because the pictures are identical
       comm_s = os.environ["LSDCHOME"] + "/runPinAlign.py " + snapshot1Name + " " + snapshot2Name
       logger.info(comm_s)
-      lines = os.popen(comm_s, cwd=getBlConfig("visitDirectory")).readlines()
+      lines = subprocess.Popen(comm_s, cwd=getBlConfig("visitDirectory")).readlines()
       logger.info("printing lines right after popen ")
       logger.info(lines)
       logger.info(" done")

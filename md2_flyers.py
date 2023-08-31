8a5ce24cd71962807eb8525630671b29d964cc60
mskinner5278 <m.skinner5278@gmail.com>
2023-08-31 15:00:12 -0400

    trying to solve missing metadata

--- md2_flyers.py
+++ md2_flyers.py
@@ -106,9 +106,79 @@ class MD2StandardFlyer():
         logger.debug("flyer unstaging")
         self.collection_params = {}
 
+    def read_configuration(self):
+        return {}
+
+    def describe_configuration(self):
+        return {}
+
+   # def collect_asset_docs(self):
+   #     for _ in ():
+   #         yield _
+
     def collect_asset_docs(self):
-        for _ in ():
-            yield _
+        asset_docs_cache = []
+
+        # Get the Resource which was produced when the detector was staged.
+        ((name, resource),) = self.detector.file.collect_asset_docs()
+
+        asset_docs_cache.append(("resource", resource))
+        self._datum_ids = DEFAULT_DATUM_DICT
+        # Generate Datum documents from scratch here, because the detector was
+        # triggered externally by the DeltaTau, never by ophyd.
+        resource_uid = resource["uid"]
+        # num_points = int(math.ceil(self.detector.cam.num_images.get() /
+        #                 self.detector.cam.fw_num_images_per_file.get()))
+
+        # We are currently generating only one datum document for all frames, that's why
+        #   we use the 0th index below.
+        #
+        # Uncomment & update the line below if more datum documents are needed:
+        # for i in range(num_points):
+
+        seq_id = self.detector.cam.sequence_id.get()
+
+        self._master_file = f"{resource['root']}/{resource['resource_path']}_{seq_id}_master.h5"
+        if not os.path.isfile(self._master_file):
+            raise RuntimeError(f"File {self._master_file} does not exist")
+
+        # The pseudocode below is from Tom Caswell explaining the relationship between resource, datum, and events.
+        #
+        # resource = {
+        #     "resource_id": "RES",
+        #     "resource_kwargs": {},  # this goes to __init__
+        #     "spec": "AD-EIGER-MX",
+        #     ...: ...,
+        # }
+        # datum = {
+        #     "datum_id": "a",
+        #     "datum_kwargs": {"data_key": "data"},  # this goes to __call__
+        #     "resource": "RES",
+        #     ...: ...,
+        # }
+        # datum = {
+        #     "datum_id": "b",
+        #     "datum_kwargs": {"data_key": "omega"},
+        #     "resource": "RES",
+        #     ...: ...,
+        # }
+
+        # event = {...: ..., "data": {"detector_img": "a", "omega": "b"}}
+
+        for data_key in self._datum_ids.keys():
+            datum_id = f"{resource_uid}/{data_key}"
+            self._datum_ids[data_key] = datum_id
+            datum = {
+                "resource": resource_uid,
+                "datum_id": datum_id,
+                "datum_kwargs": {"data_key": data_key},
+            }
+            asset_docs_cache.append(("datum", datum))
+        return tuple(asset_docs_cache)
+
+    def _extract_metadata(self, field="omega"):
+        with h5py.File(self._master_file, "r") as hf:
+            return hf.get(f"entry/sample/goniometer/{field}")[()]
 
 class MD2VectorFlyer(MD2StandardFlyer):
     def __init__(self, md2, detector=None) -> None:

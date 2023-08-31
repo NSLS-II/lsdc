a038f7002d769dab63e966c5b99f7bb01ea078dd
mskinner5278 <m.skinner5278@gmail.com>
2023-08-31 11:36:46 -0400

    move detector staging

--- daq_macros.py
+++ daq_macros.py
@@ -3699,7 +3699,6 @@ def standardDaq(currentRequest):
     flyer.detector_arm(angle_start, img_width, total_num_images, exposure_per_image, 
                      file_prefix, data_directory_name, file_number_start, x_beam, y_beam, 
                      wavelength, det_distance_m, num_images_per_file)
-    flyer.detector.stage()
     def armed_callback(value, old_value, **kwargs):
         return (old_value == 0 and value == 1)
     arm_status = SubscriptionStatus(flyer.detector.cam.armed, armed_callback, run=False)
@@ -3714,6 +3713,7 @@ def standardDaq(currentRequest):
     start_time = time.time()
     yield from bps.mv(md2.phase, 2) # TODO: Enum for MD2 phases and states
     md2.ready_status().wait(timeout=10)
+    flyer.detector.stage()
     logger.info(f"MD2 phase transition to 2-DataCollection took {time.time()-start_time} seconds.")
     flyer.update_parameters(total_num_images, angle_start, scan_range, exposure_time)
     yield from bp.fly([flyer])
@@ -3766,7 +3766,6 @@ def vectorDaq(currentRequest):
     vector_flyer.detector_arm(angle_start, img_width, total_num_images, exposure_per_image, 
                      file_prefix, data_directory_name, file_number_start, x_beam, y_beam, 
                      wavelength, det_distance_m, num_images_per_file)
-    flyer.detector.stage()
     def armed_callback(value, old_value, **kwargs):
         return (old_value == 0 and value == 1)
     arm_status = SubscriptionStatus(vector_flyer.detector.cam.armed, armed_callback, run=False)
@@ -3781,6 +3780,7 @@ def vectorDaq(currentRequest):
     start_time = time.time()
     yield from bps.mv(md2.phase, 2) # TODO: Enum for MD2 phases and states
     md2.ready_status().wait(timeout=10)
+    flyer.detector.stage()
     logger.info(f"MD2 phase transition to 2-DataCollection took {time.time()-start_time} seconds.")
     vector_flyer.update_parameters(angle_start, scan_range, exposure_time, start_y, start_z, stop_y, stop_z)
     yield from bp.fly([vector_flyer])

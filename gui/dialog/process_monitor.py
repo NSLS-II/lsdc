e7f65a4eec12248740a94841f426f5687c46bc4d
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2024-05-21 16:36:06 -0400

    Update process_monitor.py

--- gui/dialog/process_monitor.py
+++ gui/dialog/process_monitor.py
@@ -16,7 +16,7 @@ class ProcessPopup(QtWidgets.QMessageBox):
     def __init__(self, parent: "ControlMain",window_title = 'Info', main_text =  'Waiting for somethign in GUI, open more details for more info', detailed_text = 'waiting'):
         super(ProcessPopup, self).__init__(parent)
         self.setWindowTitle(window_title)
-        self.setIcon(QtWidgets.QMessageBox.question)
+        self.setIcon(QtWidgets.QMessageBox.Question)
         self.setText(main_text)
         self.setDetailedText(detailed_text)
 
